415debee02554d72d28847886f71ea9009af9ee0
SotheaH <157082641+SotheaH@users.noreply.github.com>
2024-01-19 19:25:21 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-d5_panorama-2_20240118_160850_test",
+      "name": "D5_Panorama 2_20240118_160850_test",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3522.5,
+      "initialViewParameters": {
+        "yaw": 1.5208073790202112,
+        "pitch": 0,
+        "fov": 1.0206223595689021
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6777499171437844,
+          "pitch": -0.04311189116309144,
+          "rotation": 0,
+          "target": "1-d5_panorama-5_20240118_164602_test"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-d5_panorama-5_20240118_164602_test",
+      "name": "D5_Panorama 5_20240118_164602_test",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3522.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.087422196629266,
+          "pitch": 0.05721727161806811,
+          "rotation": 0,
+          "target": "0-d5_panorama-2_20240118_160850_test"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "PHYRADA BEDROOM",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
